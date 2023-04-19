a762b731d5ae57848133aee691bd00a86e46df45
vitas <vitaszhuo@xiaoe-tech.com>
2023-04-19 21:17:42 +0800

    提交代码

--- models/user_basic.go
+++ models/user_basic.go
@@ -1,26 +1,28 @@
 package models
 
 import (
-	"gorm.io/gorm"
 	"time"
 )
 
 type UserBasic struct {
-	gorm.Model
-	Name          string
-	Password      string
-	Phone         string
-	Email         string
-	Identity      string
-	ClientIp      string
-	ClientPort    string
-	LoginTime     time.Time
-	HeartbeatTime time.Time
-	LogOutTime    time.Time
-	IsLogout      bool
-	DeviceInfo    string
+	ID            int64     `gorm:"column:id" db:"id" json:"id" form:"id"`
+	CreatedAt     time.Time `gorm:"column:created_at" db:"created_at" json:"created_at" form:"created_at"`
+	UpdatedAt     time.Time `gorm:"column:updated_at" db:"updated_at" json:"updated_at" form:"updated_at"`
+	DeletedAt     time.Time `gorm:"column:deleted_at" db:"deleted_at" json:"deleted_at" form:"deleted_at"`
+	Name          string    `gorm:"column:name" db:"name" json:"name" form:"name"`
+	Password      string    `gorm:"column:password" db:"password" json:"password" form:"password"`
+	Phone         string    `gorm:"column:phone" db:"phone" json:"phone" form:"phone"`
+	Email         string    `gorm:"column:email" db:"email" json:"email" form:"email"`
+	Identity      string    `gorm:"column:identity" db:"identity" json:"identity" form:"identity"`
+	ClientIp      string    `gorm:"column:client_ip" db:"client_ip" json:"client_ip" form:"client_ip"`
+	ClientPort    string    `gorm:"column:client_port" db:"client_port" json:"client_port" form:"client_port"`
+	LoginTime     time.Time `gorm:"column:login_time" db:"login_time" json:"login_time" form:"login_time"`
+	HeartbeatTime time.Time `gorm:"column:heartbeat_time" db:"heartbeat_time" json:"heartbeat_time" form:"heartbeat_time"`
+	LogOutTime    time.Time `gorm:"column:log_out_time" db:"log_out_time" json:"log_out_time" form:"log_out_time"`
+	IsLogout      int64     `gorm:"column:is_logout" db:"is_logout" json:"is_logout" form:"is_logout"`
+	DeviceInfo    string    `gorm:"column:device_info" db:"device_info" json:"device_info" form:"device_info"`
 }
 
-func (table *UserBasic) TableName() string {
+func (UserBasic) TableName() string {
 	return "user_basic"
 }
