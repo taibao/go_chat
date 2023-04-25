ea2ce59bd1660c6bb22191fb2e4226fed3af9255
vitas <vitaszhuo@xiaoe-tech.com>
2023-04-25 08:45:07 +0800

    自定义日志模板

--- utils/system_init.go
+++ utils/system_init.go
@@ -5,6 +5,10 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"time"
 )
 
 var DB *gorm.DB
@@ -21,7 +25,16 @@ func InitConfig() {
 
 }
 func InitMysql() {
-	db, err := gorm.Open(mysql.Open(viper.GetString("mysql.dns")), &gorm.Config{})
+	//自定义日志模板，打印SQL语句
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second, //慢sql阈值
+			LogLevel:      logger.Info, //级别
+			Colorful:      true,        //彩色
+		},
+	)
+	db, err := gorm.Open(mysql.Open(viper.GetString("mysql.dns")), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		panic("failed to connect database")
 	}
