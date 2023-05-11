411788895ae2470efccc384d7e5429d3d79f76ba
vitas <vitaszhuo@xiaoe-tech.com>
2023-05-11 08:55:14 +0800

    提交

---
+++ models/contact.go
@@ -0,0 +1,16 @@
+package models
+
+import "gorm.io/gorm"
+
+type contact struct {
+	gorm.Model
+	FormId   string //发送者
+	TargetId string //接受者
+	Type     string //消息类型 群聊 私聊 广播
+	Media    int    //消息类型 文字 图片音频
+	Content  string //消息内容
+	Pic      string
+	Url      string
+	Desc     string
+	Amount   int //其他数字统计
+}
